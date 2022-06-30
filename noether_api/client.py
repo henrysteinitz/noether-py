ced20e188c143acb5225be78c9f844e73771df71
Henry <hjsteinitz@gmail.com>
2022-06-30 12:52:00 -0400

    Add color, save, and search methods

--- noether_api/client.py
+++ noether_api/client.py
@@ -28,9 +28,25 @@ class NoetherClient:
 		return requests.get("{}/ask?{}&question={}&api_key={}".format(noether_url, data_input, question, self._api_key)).json()
 
 
+	def color(self, obj: str = None, image_url: str = None):
+		if obj is None:
+			return self.ask(image_url=image_url, question="What color is the image")
+		else:
+			return self.ask(image_url=image_url, question="What color is the {} in the image".format(obj))
+
+
 	def embed(self, text: str = None, image_url: str = None, audio_url: str = None, video_url: str = None):
 		data_input = parse_data_input(text, image_url, audio_url, video_url)
 		return requests.get("{}/embed?{}&api_key={}".format(noether_url, data_input, self._api_key)).json()
+
+
+	def save(self, text: str = None, image_url: str = None, audio_url: str = None, video_url: str = None):
+		data_input = parse_data_input(text, image_url, audio_url, video_url)
+		return requests.get("{}/save?{}&api_key={}".format(noether_url, data_input, self._api_key)).json()
+
+
+	def search(self, query: str):
+		return requests.get("{}/search?query={}&api_key={}".format(noether_url, query, self._api_key)).json()
 
 
 	def summary(self, text: str = None, image_url: str = None, audio_url: str = None, video_url: str = None):
